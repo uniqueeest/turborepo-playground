6c4e0e891c7051c12c71dd58da0a414b02a48353
Yoonjae Choi <dbswo9795@gmail.com>
2025-09-03 23:45:32 +0900

    init project

---
+++ apps/web/src/index.ts
@@ -0,0 +1,3 @@
+import { greet } from 'shared';
+
+console.log(greet('Web'));
---
+++ packages/shared/src/index.ts
@@ -0,0 +1,6 @@
+/**
+ * greet 함수는 전달된 이름을 포맷팅하여 반갑다는 문자열을 반환합니다.
+ */
+export const greet = (name: string): string => {
+  return `Hello, ${name}!`;
+};
